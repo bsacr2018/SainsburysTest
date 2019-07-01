d1053ab77c415c87f9d20e8fa7115cf7d35fae4b
bsacr2018 <45632790+bsacr2018@users.noreply.github.com>
2019-07-01 23:53:33 +0100

    Initial project files

---
+++ src/main/java/com/robinson/projects/sainsburystest/ScraperTask.java
@@ -0,0 +1,9 @@
+package com.robinson.projects.sainsburystest;
+
+public class ScraperTask {
+	
+	public static void main(String[] args) {
+		System.out.println("main");
+	}
+
+}
---
+++ src/test/java/com/robinson/projects/sainsburystest/ScraperTaskTest.java
@@ -0,0 +1,14 @@
+package com.robinson.projects.sainsburystest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ScraperTaskTest {
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+
+}
