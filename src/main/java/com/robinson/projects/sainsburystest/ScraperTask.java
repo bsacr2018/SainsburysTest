a7551a7f17c8fd1256e930017fe65611786ee4af
bsacr2018 <45632790+bsacr2018@users.noreply.github.com>
2019-07-02 22:31:10 +0100

    First set of tests for ensuring that Result and Total are handled appropriately, when generating JSON.

--- src/main/java/com/robinson/projects/sainsburystest/ScraperTask.java
+++ src/main/java/com/robinson/projects/sainsburystest/ScraperTask.java
@@ -1,9 +1,30 @@
 package com.robinson.projects.sainsburystest;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import com.robinson.projects.sainsburystest.jsonhandler.*;
+
 public class ScraperTask {
-	
+
 	public static void main(String[] args) {
-		System.out.println("main");
+		List<Product> products = new ArrayList<Product>();
+		
+		//Establish Products	
+		Product blueberries = new Product("http://", "blueberries", 1.75);		
+		blueberries.setDescription("By Sainsburys");
+		blueberries.setKcal_per_100g(Optional.of(40));		
+		products.add(blueberries);
+
+		DisplayableProduct dps = new DisplayableProduct();
+		dps.add(blueberries);
+		
+		// Display indivdual results
+		List<Result> rs = dps.getResults();
+		for (Result r : rs) {
+			System.out.println(r.toJSON());
+		}
 	}
 
 }
